bc602e921b1fd64cc00ee703fb96aff8396fb5f3
swiderskis <66870324+swiderskis@users.noreply.github.com>
2023-04-15 10:25:39 +0100

    Make login theme dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import { type AppType } from "next/app";
-
 import { api } from "~/utils/api";
-
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+      {...pageProps}
+    >
       <Component {...pageProps} />
     </ClerkProvider>
   );
